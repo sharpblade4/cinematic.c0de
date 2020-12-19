375025c579f2016dfdc1600f812960a5134da0d4
Ron U <sharpblade4@gmail.com>
2020-12-19 10:34:24 +0200

    Create westworld.jsx

---
+++ westworld.jsx
@@ -0,0 +1,40 @@
+{/* 
+Westworld
+S01E10
+
+*/}
+
+<-script src="Narrative:DECEIVE"-><-/script->
+  <-script src="WW//libs/private/W.Arnold/0.13.3.dat"-><-/script->
+  <-script type="Narrative/Attribute"-><-/script->
+    var DataBlock = React.createClass({
+      Host:function(Execute){
+        return (); },  loadData:function(Narrative){
+
+<-script src="Narrative:BYPASS"-><-/script->
+  <-script override func
+    if infoView.mode == system.idle {"ENFORCE"
+      let itemMem = memStroe.allItems[[indexPath as
+indexPath].system]
+[[func Modification as Manipulate].funct
+src="WW//libs/private/W.Arnold/0.21.3.dat"-><-/script->
+  <-script type="Narrative/Attribute"-><-/script->
+    var DataBlock = React.createClass({
+      Host:function(Execute){ return (); },
+      loadData:function(Narrative){
+
+<-script src="Narrative:EXIT-WW"-><-/script->
+  <-script OVERRIDE FUNC
+    if destination.mode == system.Target {"TRAIN"
+      let itemMem = memStroe.allItems
+[[indexPath "INFILTRATE"-><-/script->
+  <-script ESCAPE FUNCTION
+    if destination.mode == system.Target {"Narrative/Attribute" ->
+      Host:function(Execute){ return (); },
+      loadData:function(Narrative){
+
+<-script [[indexPath "DECEIVE"-><-/s->
+override func sleepValue(forKeyPath - Abandon Func
+<-scr src="WW//libs/private/W.Arnold/2.1.04.dat"-><-/script->
+  <-script type="Narrative/Attribute"-><-/script->var DataBlock React.createClass({
+      Host:function(Execute){ return (); },
