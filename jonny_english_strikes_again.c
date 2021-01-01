6966190d4e874eaf012888812605c3bacf067eb1
Ron U <sharpblade4@gmail.com>
2021-01-01 11:21:40 +0200

    Create jonny_english_strikes_again.c

---
+++ jonny_english_strikes_again.c
@@ -0,0 +1,38 @@
+/**
+  Jonny English Strikes Again - 01:05:35
+  
+  Sources:
+    - https://www.programmingsimplified.com/c-program-generate-random-numbers
+    - https://github.com/torvalds/linux/commit/30639b6af85a92491b22dd14c17b14ca11da60e6#diff-1512c1aba152e691ee057b387bf535d86bba869a70ce7974ef9fe5ebe0118f1c
+*/
+
+// ----------------------------------------------
+// Primary monitor:
+// ----------------------------------------------
+
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+int main()
+{
+   int n, max, num, c;
+
+   printf("Enter the number of random 
+    numbers you
+   
+   
+
+
+// ----------------------------------------------
+// Secondary monitor:
+// ----------------------------------------------
+
+struct group_info 
+init_groups = { 
+.usage = 
+ATOMIC_INIT(2) };
+
+struct group_info 
+*groups_alloc(int 
+gidsetsize
