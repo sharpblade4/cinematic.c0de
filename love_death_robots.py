ea4f81614f22c1fcbc8363568a414246b047f9f9
Ron U <sharpblade4@gmail.com>
2021-07-23 11:34:51 +0300

    Create love_death_robots.py

---
+++ love_death_robots.py
@@ -0,0 +1,26 @@
+"""
+
+Love Death + Robots   -   s02e'Automated Customer Service'
+
+Potential Sources:
+  - https://www.pyimagesearch.com/2019/03/11/liveness-detection-with-opencv/
+  - https://nanonets.com/blog/table-extraction-deep-learning/
+  
+"""
+
+# Search for the pattern and replace it with the given string
+re.sub(pattern, replace, string)
+
+detections = net.forward()
+
+while True:
+# grab data video,resize
+idx = int.82%
+box = detections(0, 0, i, 7) * np.array(w, h, w, h)
+(startX, startY, endX, endY) = box.astype('int')
+
+target_lock = "scanning"
+"human"/"homo sapiens","Jeanette lafayette",
+"vacuubot"
+
+# 33 36 51, 112 16 55
